f0780c03fb2f0ad9738620e2f7cf3fa8b707c5ad
OmMahen <102027819+OmMahen@users.noreply.github.com>
2023-05-23 12:32:19 +0800

    Create Routing for Lost Items Page
    Buat routing untuk halaman yang nampilin semua barang yang hilang

---
+++ src/scripts/views/pages/lostitems.js
@@ -0,0 +1,13 @@
+const LostItems = {
+  async render() {
+    return `
+          <h2>Lost Items page</h2>
+        `;
+  },
+
+  async afterRender() {
+    // Fungsi ini akan dipanggil setelah render()
+  },
+};
+
+export default LostItems;
