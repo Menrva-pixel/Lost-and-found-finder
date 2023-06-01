602aa44ff192a9a821fd91f990f7d21f2aba6c2b
Vicky Pratama Setia Mahardika <92003802+VickyPratama87@users.noreply.github.com>
2023-06-01 08:47:32 +0700

    Mengupdate Navbar, Home, Footer, Lost-item Service dan Found-item Service
    Navbar : Merubah style link a dan button about
    Home : Merubah ukuran font, style button, dan layouting pada function-section
    Lost-item service dan Found-item service : Merubah style form
    Footer : Merubah style link a (Our Team)

--- src/scripts/views/pages/lost-item-service.js
+++ src/scripts/views/pages/lost-item-service.js
@@ -4,25 +4,40 @@ import '../components/_lost-form';
 import bgImg from '../../../public/images/img-finder.png';
 
 const LostItemService = {
+  connectedCallback() {
+    this.render();
+    this.addEventListener();
+  },
+
+  addEventListener() {
+    const syarat = this.querySelector('.syarat');
+
+    syarat.addEventListener('click', () => {
+      window.location.hash = '#/terms-of-use';
+    });
+  },
+
   async render() {
     return `
     <div class="hero">
-    <div class="hero-text">
-       <h3>Halo!</h3>
-      <h1>Kehilangan Barang?</h1>
-      <p>Buat laporan agar orang lain atau finder dapat menemukan barang mu yang hilang!</p>
+      <div class="hero-text">
+        <h3>Halo!</h3>
+        <h1>Kehilangan Barang?</h1>
+        <p>Buat laporan agar orang lain atau finder dapat menemukan barang mu yang hilang!</p>
+      </div>
+      <div class="bg-image">
+        <img src="${bgImg}" alt="Hero Image">
+      </div>
     </div>
-    <div class="bg-image">
-      <img src="${bgImg}" alt="Hero Image">
+
+    <div id="information-section">
+        <p>Harap pastikan untuk membaca dan memahami <a class="syarat">Syarat dan Ketentuan</a> website kami guna menghindari penggunaan yang tidak tepat atau melanggar prosedur. 
+        Setelah Anda memahami syarat dan ketentuan tersebut, silakan manfaatkan fitur-fitur website kami sebaik mungkin. Kami berharap dengan tulus agar barang-barang yang hilang segera ditemukan dan dikembalikan kepada pemiliknya. 
+        Terima kasih atas kerjasamanya!</p>
     </div>
-  </div>
- <div id="information-section">
-    <p>Harap pastikan untuk membaca dan memahami <b>Syarat dan Ketentuan</b> website kami guna menghindari penggunaan yang tidak tepat atau melanggar prosedur. 
-    Setelah Anda memahami syarat dan ketentuan tersebut, silakan manfaatkan fitur-fitur website kami sebaik mungkin. Kami berharap dengan tulus agar barang-barang yang hilang segera ditemukan dan dikembalikan kepada pemiliknya. 
-    Terima kasih atas kerjasamanya!</p>
- </div>
-  <div id="form-section">
-    <lost-form></lost-form>
+
+    <div id="form-section">
+      <lost-form></lost-form>
     </div>
     `;
   },
