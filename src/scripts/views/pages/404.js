e080c74c1001704bae9367fa104cdbc64030ff80
Herdyanto26 <71887357+Menrva-pixel@users.noreply.github.com>
2023-06-09 17:57:56 +0700

    leftover patch
    re applying 404 function

--- src/scripts/views/pages/404.js
+++ src/scripts/views/pages/404.js
@@ -3,7 +3,7 @@ const Error = {
   async render() {
     return `
       <div id="bad-request">
-        <img src="images/404.png">
+        <img src="images/404.png" alt="404 Bad Request">
           <div class="bd-desc">
             <p>Ooopss.. page not found</p>
             <p>You will be redirected to the homepage shortly.</p>
@@ -14,7 +14,19 @@ const Error = {
   },
 
   async afterRender() {
-},
+    const countdownElement = document.getElementById('countdown');
+    let countdown = 5;
+
+    const countdownInterval = setInterval(() => {
+      countdownElement.textContent = countdown;
+      countdown--;
+
+      if (countdown < 0) {
+        clearInterval(countdownInterval);
+        window.location.href = '#';
+      }
+    }, 1000);
+  },
 };
 
 export default Error;
