dfc59ce3334dd88f63ef9f3e822f1e50d4d53e81
bherdyanto26 <bherdyanto26@gmail.com>
2023-06-10 17:45:21 +0700

    detail item update
    new detail item ui
    moving form style code style -> _form.css
    several patch

--- src/scripts/views/components/_information-section.js
+++ src/scripts/views/components/_information-section.js
@@ -1,4 +1,7 @@
 /* eslint-disable class-methods-use-this */
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
 import LostAndFoundAPI from '../../data/lost-and-found-api';
 import { createLostItemCard } from '../template/template-creator';
 
@@ -6,17 +9,44 @@ class information extends HTMLElement {
   connectedCallback() {
     this.render();
     this.afterRender();
+    this.initializeAOS();
+    this.handleScroll();
   }
 
   render() {
     this.innerHTML = `
         <h2>Informasi Barang Hilang</h2>
         <button id="scrollLeft"><i class="bi bi-caret-left-fill"></i></button>
-        <div class="containerCard">
+        <div class="containerCard" data-aos="fade-up">
         </div>
         <button id="scrollRight"><i class="bi bi-caret-right-fill"></i></button>
         <h3><a href="#/list-item-service">Lihat Semua...</a></h3>
         `;
+  }
+
+  initializeAOS() {
+    AOS.init({
+      offset: 200,
+      duration: 500,
+      easing: 'ease-in-sine',
+      delay: 100,
+      once: false,
+      mirror: true,
+    });
+  }
+
+  handleScroll() {
+    let lastScrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    window.addEventListener('scroll', () => {
+      const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+      const isScrollingDown = scrollTop > lastScrollTop;
+
+      if (isScrollingDown) {
+        AOS.refresh();
+      }
+
+      lastScrollTop = scrollTop;
+    });
   }
 
   async afterRender() {
